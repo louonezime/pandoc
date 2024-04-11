cc28a85cbc0b810ddc3a7971545e473cbaff7783
Lou Onezime <lou.onezime@epitech.eu>
2024-04-11 17:28:37 +0200

    feat(parser): beginning of test for parser

---
+++ test/ParserSpec.hs
@@ -0,0 +1,20 @@
+{-
+-- EPITECH PROJECT, 2024
+-- pandoc
+-- File description:
+-- Parser
+-}
+
+module ParserSpec (spec) where
+
+import Test.Hspec
+import Control.Applicative (Alternative (..))
+
+import Parsing
+
+spec :: Spec
+spec = do
+    describe "parseChar" $ do
+        context "when a char that is in string" $ do
+            it "returns a tuple of the char and the rest of string" $
+                runParser (parseChar 'a' <|> parseChar 'b') "abcd" `shouldBe` Right ('a', "bcd")
