4e1f402678d0a075df8d9ee782d5f1097a0169a1
MartinFillon <martin.fillon@hotmail.com>
2024-04-15 12:50:50 +0200

    feat: add basic text printing

--- app/display/Json.hs
+++ app/display/Json.hs
@@ -1,11 +1,17 @@
+{-
+-- EPITECH PROJECT, 2024
+-- pandoc
+-- File description:
+-- JsonDisplayer
+-}
 
 module Json (renderJson) where 
 
-import Document (Document (..), Header (..))
+import Document (Document (..), Header (..), Entry (..))
 import System.IO (Handle, hPutStrLn, hPutStr)
 
 renderJson :: Document -> String
-renderJson (Document hdr _) = '{':(renderHeader hdr) ++ "}"
+renderJson (Document hdr bd) = '{':(renderHeader hdr) ++ (renderBody bd) ++ "}"
 
 renderHeader :: Header -> String
 renderHeader (Header t a d) = "\"header\":{" ++ (renderTitle t) ++ (renderAuthor a) ++ (renderDate d) ++"}"
@@ -18,6 +24,19 @@ renderTitle :: String -> String
 renderTitle t = "\"title\":\""++t++"\""
 
 renderDate :: Maybe String -> String
-renderDate (Just date) = ",\"date\":\"" ++ date ++ "\""
+renderDate (Just d) = ",\"date\":\"" ++ d ++ "\""
 renderDate Nothing = ""
 
+append :: String -> String -> String
+append s res = res ++ ',':s
+
+renderBody :: [Entry] -> String
+renderBody e = ",\"body\":["++ (renderBodyContent e) ++ "]"
+
+renderBodyContent :: [Entry] -> String
+renderBodyContent [] = ""
+renderBodyContent (x:xs) = (renderEntry x) ++ (foldr append [] (map renderEntry xs))
+
+renderEntry :: Entry -> String
+renderEntry (Text s) = '"':s ++ "\""
+renderEntry _ = "null"
