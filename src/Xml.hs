b2f2a8812f90111aa5c471733a972a48264b4170
Lou Onezime <lou.onezime@epitech.eu>
2024-04-23 18:04:57 +0200

    fix(xml): remove useless lines

--- src/Xml.hs
+++ src/Xml.hs
@@ -25,32 +25,32 @@ parseTag :: String -> Parser String
 parseTag name = parseChar '<' *> parseChar name *> parseMany (parseAttribute name) <* parseChar '>'
 
 parseTextContent :: Parser String
-parseTextContent = parseSome (parseSomeChar (['a'..'z'] ++ ['A'..'Z'] ++ " ,.!?")) Adjust as needed
+parseTextContent = parseSome (parseSomeChar (['a'..'z'] ++ ['A'..'Z'] ++ " ,.!?"))
 
-parseXMLElement :: String -> Parser Entry
-parseXMLElement "paragraph" = Paragraph <$> (parseTag "paragraph" *> parseTextContent <* parseTag "/paragraph")
-parseXMLElement "bold" = Bold <$> (parseTag "bold" *> parseTextContent <* parseTag "/bold")
-parseXMLElement "italic" = Italic <$> (parseTag "italic" *> parseTextContent <* parseTag "/italic")
-parseXMLElement "code" = Code <$> (parseTag "code" *> parseTextContent <* parseTag "/code")
-parseXMLElement "codeblock" = CodeBlock <$> (parseTag "codeblock" *> parseTextContent <* parseTag "/codeblock")
-parseXMLElement "list" = List <$> (parseTag "list" *> parseMany (parseXMLElement "paragraph") <* parseTag "/list")
-parseXMLElement "link" = do
-  url <- parseTag "link" *> parseAttribute "url" <* parseChar '>'
-  content <- parseTextContent <* parseTag "/link"
-  return (Link url [Paragraph content])
-parseXMLElement "image" = do
-  url <- parseTag "image" *> parseAttribute "url" <* parseChar '>'
-  altText <- parseTextContent <* parseTag "/image"
-  return (Image url [Paragraph altText])
-parseXMLElement "section" = do
-  title <- parseTag "section" *> parseAttribute "title" <* parseChar '>'
-  content <- parseMany parseXML <* parseTag "/section"
-  return (Section title content)
+-- parseXMLElement :: String -> Parser Entry
+-- parseXMLElement "paragraph" = Paragraph <$> (parseTag "paragraph" *> parseTextContent <* parseTag "/paragraph")
+-- parseXMLElement "bold" = Bold <$> (parseTag "bold" *> parseTextContent <* parseTag "/bold")
+-- parseXMLElement "italic" = Italic <$> (parseTag "italic" *> parseTextContent <* parseTag "/italic")
+-- parseXMLElement "code" = Code <$> (parseTag "code" *> parseTextContent <* parseTag "/code")
+-- parseXMLElement "codeblock" = CodeBlock <$> (parseTag "codeblock" *> parseTextContent <* parseTag "/codeblock")
+-- parseXMLElement "list" = List <$> (parseTag "list" *> parseMany (parseXMLElement "paragraph") <* parseTag "/list")
+-- parseXMLElement "link" = do
+--   url <- parseTag "link" *> parseAttribute "url" <* parseChar '>'
+--   content <- parseTextContent <* parseTag "/link"
+--   return (Link url [Paragraph content])
+-- parseXMLElement "image" = do
+--   url <- parseTag "image" *> parseAttribute "url" <* parseChar '>'
+--   altText <- parseTextContent <* parseTag "/image"
+--   return (Image url [Paragraph altText])
+-- parseXMLElement "section" = do
+--   title <- parseTag "section" *> parseAttribute "title" <* parseChar '>'
+--   content <- parseMany parseXML <* parseTag "/section"
+--   return (Section title content)
 
-parseXML :: Parser Document
-parseXML = do
-  parseTag "document"
-  header <- parseXMLElement "header"
-  body <- parseXMLElement "body"
-  parseTag "/document"
-  return (Document header [body])
+-- parseXML :: Parser Document
+-- parseXML = do
+--   parseTag "document"
+--   header <- parseXMLElement "header"
+--   body <- parseXMLElement "body"
+--   parseTag "/document"
+--   return (Document header [body])
