bc0e9bff4caffd1596aac59b07e70d56955e78ed
MartinFillon <martin.fillon@hotmail.com>
2024-04-24 12:43:42 +0200

    feat: add header parsing with lib

--- src/Markdown.hs
+++ src/Markdown.hs
@@ -5,9 +5,15 @@
 -- pandoc
 -}
 
-import Data.List
+module Markdown (parseMarkdown) where
+
 import Data.Char
 import Data.Either
+import Data.List
+
+import Control.Applicative ((<|>))
+import Document (Document (..), Header (..))
+import Parsing
 
 data MarkdownElement
     = MarkdownHeader [MarkdownElement]
@@ -29,31 +35,74 @@ data MarkdownElement
 type MarkdownContent = [MarkdownElement]
 
 parseCodeBlock :: [String] -> MarkdownContent
-parseCodeBlock ("```\n":rest) = [] -- end of code block
-parseCodeBlock (line:rest) = MarkdownCodeBlock [line] : parseCodeBlock rest
+parseCodeBlock ("```\n" : rest) = [] -- end of code block
+parseCodeBlock (line : rest) = MarkdownCodeBlock [line] : parseCodeBlock rest
 
 isCodeBlock :: String -> Either String MarkdownElement
 isCodeBlock str
     | str == "```\n" = Left "End of code block"
     | otherwise = Right (MarkdownCodeBlock [str])
 
-parseHeader :: [String] -> MarkdownContent
-parseHeader ("---\n":rest) = [] -- end of header 
-parseHeader (line:rest)
-    | "title: " `isPrefixOf` line = MarkdownTitle (drop 7 line) : parseHeader rest
-    | "author: " `isPrefixOf` line = MarkdownAuthor (Just $ drop 8 line) : parseHeader rest
-    | "date: " `isPrefixOf` line = MarkdownDate (Just $ drop 6 line) : parseHeader rest
-    | otherwise = parseHeader rest
-parseHeader [] = []
+-- parseHeader :: [String] -> MarkdownContent
+-- parseHeader ("---\n" : rest) = [] -- end of header
+-- parseHeader (line : rest)
+--     | "title: " `isPrefixOf` line = MarkdownTitle (drop 7 line) : parseHeader rest
+--     | "author: " `isPrefixOf` line = MarkdownAuthor (Just $ drop 8 line) : parseHeader rest
+--     | "date: " `isPrefixOf` line = MarkdownDate (Just $ drop 6 line) : parseHeader rest
+--     | otherwise = parseHeader rest
+-- parseHeader [] = []
+
 -- parseHeader ["title: Syntaxe MARKDOWN", "author: John Doe", "date: 2024-04-23"]
 
-isHeader :: String -> Either String MarkdownElement
-isHeader str
-    | str == "---\n" = Left "End of header"
-    | otherwise = Right (MarkdownHeader (parseHeader [str]))
+-- isHeader :: String -> Either String MarkdownElement
+-- isHeader str
+--     | str == "---\n" = Left "End of header"
+--     | otherwise = Right (MarkdownHeader (parseHeader [str]))
 
-parseMarkdown :: [String] -> MarkdownContent
-parseMarkdown = rights . concatMap (\str -> [isHeader str, isCodeBlock str])
+-- parseMarkdown :: [String] -> MarkdownContent
+-- parseMarkdown = rights . concatMap (\str -> [isHeader str, isCodeBlock str])
+
+createDocFromHeader :: Header -> Document
+createDocFromHeader hd = Document hd []
+
+parseMarkdown :: Parser Document
+parseMarkdown = createDocFromHeader <$> parseHeader (Header "" Nothing Nothing)
+
+parseHeaderDash :: Parser String
+parseHeaderDash = parseBetween "---\n"
+
+parseHeader :: Header -> Parser Header
+parseHeader hd = Parser $ \str ->
+    case runParser parseHeaderDash str of
+        Right (x, xs) -> Right (parseHeaderFields (lines x) hd, xs)
+        _ -> Left "Header Not found"
+
+parseHeaderFields :: [String] -> Header -> Header
+parseHeaderFields [] hd = hd
+parseHeaderFields (x : xs) hd = case runParser (parseHeaderField hd) x of
+    Right (r, "") -> parseHeaderFields xs r
+    _ -> hd
+
+parseHeaderField :: Header -> Parser Header
+parseHeaderField hd = Parser $ \str ->
+    case runParser parseField str of
+        Right ("title: ", "") -> Left "No title specified"
+        Right ("title: ", z) -> Right (hd {title = z}, "")
+        Right ("date: ", z) -> Right (hd {date = Just z}, "")
+        Right ("author: ", z) -> Right (hd {author = Just z}, "")
+        _ -> Left "No field found"
+
+parseField :: Parser String
+parseField = parseTitle <|> parseDate <|> parseAuthor
+
+parseTitle :: Parser String
+parseTitle = parseAfter "title: "
+
+parseDate :: Parser String
+parseDate = parseAfter "date: "
+
+parseAuthor :: Parser String
+parseAuthor = parseAfter "author: "
 
 -- parseMarkdown exampleMarkdown
 -- let exampleMarkdown = ["---\n", "title: Example", "author: John Doe", "date: 2024-04-23",
--- src/Parsing.hs
+++ src/Parsing.hs
@@ -19,11 +19,16 @@ module Parsing (
     parseTuple,
     parseQuotes,
     parseSeparators,
+    parseNonStr,
+    parseAfter,
+    parseBetween,
+    parseBefore,
     Parser (..),
 ) where
 
 import Control.Applicative (Alternative (..))
 import Control.Monad ((>=>))
+import Data.List (isPrefixOf)
 
 newtype Parser a = Parser
     { runParser :: String -> Either String (a, String)
@@ -125,3 +130,24 @@ parseNonStr str = Parser $ \s ->
 
 parseSeparators :: Parser String
 parseSeparators = parseSome (parseAnyChar " \t\n")
+
+parseAfter :: String -> Parser String
+parseAfter str = Parser $ \s ->
+    case str `isPrefixOf` s of
+        True -> Right (str, drop (length str) s)
+        False -> Left (str ++ ": not prefix")
+
+parseBefore :: String -> Parser String
+parseBefore str = Parser $ \s ->
+    case subStrIdx s str 0 of
+        -1 -> Left (str ++ ": not a suffix")
+        n -> Right (take n s, drop (n + length str) s)
+
+subStrIdx :: String -> String -> Int -> Int
+subStrIdx "" _ _ = -1
+subStrIdx s target n
+    | take (length target) s == target = n
+    | otherwise = subStrIdx (tail s) target (n + 1)
+
+parseBetween :: String -> Parser String
+parseBetween start = parseAfter start >>= parseBefore
