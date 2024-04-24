11e92244fe49fe31c8c29d112d471aa2115790fa
Lou Onezime <lou.onezime@epitech.eu>
2024-04-24 20:34:26 +0200

    feat(xml): merde

--- src/ParseXml.hs
+++ src/ParseXml.hs
@@ -55,3 +55,34 @@ parseHeaderTag header = Parser $ \str ->
             _ -> Left "No header field found"
         Right (xs, "") -> runParser (parseTitle header) str
         _ -> Left "Invalid header"
+
+-- parseTextContent :: Parser String
+-- parseTextContent = parseSome (parseSomeChar (['a'..'z'] ++ ['A'..'Z'] ++ " ,.!?"))
+
+-- parseXMLElement :: String -> Parser Entry
+-- parseXMLElement "paragraph" = Paragraph <$> (parseTag "paragraph" *> parseTextContent <* parseTag "/paragraph")
+-- parseXMLElement "bold" = Bold <$> (parseTag "bold" *> parseTextContent <* parseTag "/bold")
+-- parseXMLElement "italic" = Italic <$> (parseTag "italic" *> parseTextContent <* parseTag "/italic")
+-- parseXMLElement "code" = Code <$> (parseTag "code" *> parseTextContent <* parseTag "/code")
+-- parseXMLElement "codeblock" = CodeBlock <$> (parseTag "codeblock" *> parseTextContent <* parseTag "/codeblock")
+-- parseXMLElement "list" = List <$> (parseTag "list" *> parseMany (parseXMLElement "paragraph") <* parseTag "/list")
+-- parseXMLElement "link" = do
+--   url <- parseTag "link" *> parseAttribute "url" <* parseChar '>'
+--   content <- parseTextContent <* parseTag "/link"
+--   return (Link url [Paragraph content])
+-- parseXMLElement "image" = do
+--   url <- parseTag "image" *> parseAttribute "url" <* parseChar '>'
+--   altText <- parseTextContent <* parseTag "/image"
+--   return (Image url [Paragraph altText])
+-- parseXMLElement "section" = do
+--   title <- parseTag "section" *> parseAttribute "title" <* parseChar '>'
+--   content <- parseMany parseXML <* parseTag "/section"
+--   return (Section title content)
+
+-- parseXML :: Parser Document
+-- parseXML = do
+--    parseChevron "document"
+--    header <- parseXMLElement "header"
+--    body <- parseXMLElement "body"
+--    parseEndChevron "document"
+--    return (Document header [body])
