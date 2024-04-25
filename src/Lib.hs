7f9b69899a21942980317093a5e19a90c5d03dc1
MartinFillon <martin.fillon@hotmail.com>
2024-04-25 09:54:07 +0200

    feat: start linking

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,7 +5,10 @@
 -- Lib
 -}
 
-module Lib (Options (Options), options, defaultOptions) where
+module Lib (Options, options, defaultOptions, runPandoc) where
+
+import Data.List (isSuffixOf)
+import Document (Document (..), Header (..))
 
 data Format = Markdown | JSON | XML deriving (Show, Enum)
 
@@ -33,13 +36,22 @@ defaultOptions =
         }
 
 options :: Options -> [String] -> Either String Options
-options opt [] = Right opt
+options opt [] =
+    Right (opt {iformat = getInputFormat (iformat opt) (input opt)})
 options opt ("-o" : out : xs) = options opt {output = Just out} xs
 options opt ("-i" : inp : xs) = options opt {input = Just inp} xs
 options opt ("-f" : f : xs) = options opt {iformat = formatFromString f} xs
 options opt ("-e" : f : xs) = options opt {oformat = formatFromString f} xs
 options _ ("--help" : _) = Left help
 options _ (opt : _) = Left (opt ++ ": unrecognized option see --help.")
+
+getInputFormat :: Either String Format -> Maybe String -> Either String Format
+getInputFormat (Right f) _ = Right f
+getInputFormat (Left _) (Just s)
+    | ".md" `isSuffixOf` s = Right Markdown
+    | ".xml" `isSuffixOf` s = Right XML
+    | ".json" `isSuffixOf` s = Right JSON
+getInputFormat _ _ = Left "No format recognized"
 
 help :: String
 help =
@@ -48,3 +60,13 @@ help =
     \   -f      output format (xml, markdown, json)\n\
     \   -o      output file\n\
     \   -e      input format (xml, markdown, json)"
+
+dumpPandoc :: Document -> Format -> String
+dumpPandoc _ XML = ""
+dumpPandoc _ JSON = ""
+dumpPandoc _ Markdown = ""
+
+runPandoc :: String -> Format -> Format -> String
+runPandoc _ XML = dumpPandoc (Document (Header "test" Nothing Nothing) [])
+runPandoc _ JSON = dumpPandoc (Document (Header "test" Nothing Nothing) [])
+runPandoc _ Markdown = dumpPandoc (Document (Header "test" Nothing Nothing) [])
