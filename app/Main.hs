aa6c96745e5c3ef56ba7cc739a4ebc36d0870b9d
MartinFillon <martin.fillon@hotmail.com>
2024-04-25 12:22:50 +0200

    refactor: removed do Block and coding-style

--- app/Main.hs
+++ app/Main.hs
@@ -9,28 +9,40 @@ module Main (main) where
 
 import System.Environment (getArgs)
 import System.Exit (ExitCode (ExitFailure), exitWith)
-import System.IO (Handle, hPutStrLn, stderr)
+import System.IO (Handle, hClose, hPutStrLn, stderr)
 
-import Lib (Options (Options), defaultOptions, help, options, runPandoc)
+import Data.Functor
+
+import Lib (
+    Format,
+    Options (Options),
+    defaultOptions,
+    help,
+    optionsParser,
+    runPandoc,
+ )
 
 import File (getInput, getOutput)
 
-displayResult :: Handle -> Either String String -> IO ()
-displayResult _ (Left e) = hPutStrLn stderr e >> exitWith (ExitFailure 84)
-displayResult h (Right s) = hPutStrLn h s
+displayResult :: Either String String -> Handle -> IO ()
+displayResult (Left e) _ = hPutStrLn stderr e >> exitWith (ExitFailure 84)
+displayResult (Right s) h = hPutStrLn h s >> hClose h
 
-prepareAndRun :: Options -> IO ()
-prepareAndRun (Options o (Just i) (Right ifmt) (Right ofmt)) = do
-    inpt <- getInput i
-    handle <- getOutput o
-    displayResult handle (runPandoc ifmt ofmt inpt)
+prepareAndRun :: Maybe String -> String -> Format -> Format -> IO ()
+prepareAndRun o i ifmt ofmt =
+    getInput i
+        >>= (\s -> getOutput o >>= displayResult s) . runPandoc ifmt ofmt
 
 checkErrors :: Either String Options -> IO ()
-checkErrors (Right (Options _ Nothing _ _)) = hPutStrLn stderr help >> exitWith (ExitFailure 84)
-checkErrors (Right (Options _ _ (Left e) _)) = hPutStrLn stderr e >> exitWith (ExitFailure 84)
-checkErrors (Right (Options _ _ _ (Left e))) = hPutStrLn stderr e >> exitWith (ExitFailure 84)
-checkErrors (Right o) = prepareAndRun o
+checkErrors (Right (Options _ Nothing _ _)) =
+    hPutStrLn stderr help >> exitWith (ExitFailure 84)
+checkErrors (Right (Options _ _ (Left e) _)) =
+    hPutStrLn stderr e >> exitWith (ExitFailure 84)
+checkErrors (Right (Options _ _ _ (Left e))) =
+    hPutStrLn stderr e >> exitWith (ExitFailure 84)
+checkErrors (Right (Options o (Just i) (Right ifmt) (Right ofmt))) =
+    prepareAndRun o i ifmt ofmt
 checkErrors (Left s) = hPutStrLn stderr s >> exitWith (ExitFailure 84)
 
 main :: IO ()
-main = getArgs >>= checkErrors . options defaultOptions
+main = getArgs >>= checkErrors . optionsParser defaultOptions
