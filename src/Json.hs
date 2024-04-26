75d9a48629fae4a7d5335b1d6193d530c7c4fcdc
SEPHORAHANIAMBOSSOU <sephorah.aniambossou@epitech.eu>
2024-04-26 18:00:28 +0200

    feat(json): parse json header

--- src/Json.hs
+++ src/Json.hs
@@ -10,11 +10,18 @@ module Json where
 import Parsing
 import Document
 import Control.Applicative ((<|>), optional)
+import Data.List
 
 parseJsonEntry :: Parser Entry
 parseJsonEntry = (optional (parseSeparators) *>
     (parseJsonString <|> parseJsonArray <|> parseJsonObject) <*
     optional (parseSeparators) <* optional (parseChar ','))
+
+-- parseJsonEntries :: Parser [Entry]
+-- parseJsonEntries = Parser $ \s -> case runParser
+--     (parseSome parseJsonEntry) s of
+--         Right (x, str) -> Right (x, str) 
+--         Left e -> Left e
 
 parseJsonString :: Parser Entry
 parseJsonString = Parser $ \s -> case runParser parseQuotes s of
@@ -42,10 +49,58 @@ parseJsonObject = Parser $ \s -> case runParser
 
 parseJsonObjectContent :: Parser [(String, [Entry])]
 parseJsonObjectContent = parseSome ((optional (parseSeparators) *>
-    parseKeyValue <* optional (parseSeparators) <* optional (parseChar ',')))
+    parseKeyValue <* optional (parseSeparators) <* optional (parseChar ','))
+    <* optional (parseSeparators))
 
 parseKeyValue :: Parser (String, [Entry])
 parseKeyValue = optional (parseSeparators) *>
     parseAnd parseQuotes (parseChar ':') <* optional (parseSeparators)
     >>= \(x, _) -> optional (parseSeparators) *> parseJsonEntry
     >>= \(y) -> return (x, y:[])
+
+defaultHeader :: Header
+defaultHeader = Header
+    { title = "",
+      author = Nothing,
+      date = Nothing
+    }
+
+parseJsonHeader :: Header -> Parser Header
+parseJsonHeader header = Parser $ \s -> case runParser
+    (parseJsonHeaderKey header) s of
+        Right (x, str) -> Right (parseJsonHeaderTags header str, str)
+        Left e -> Left e
+
+parseJsonHeaderKey :: Header -> Parser String
+parseJsonHeaderKey header = parseSome (parseChar '{' *>
+    (optional (parseSeparators) *>
+    parseAfter "\"header\"" <* optional (parseSeparators))
+    <* optional (parseSeparators) *> (parseChar ':'))
+
+parseJsonHeaderTags :: Header -> String -> Header
+parseJsonHeaderTags header "" = header
+parseJsonHeaderTags header str = case runParser (parseJsonDateTag header
+    <|> parseJsonAuthorTag header <|> parseJsonTitleTag header) str of
+        Right (x, xs) -> parseJsonHeaderTags x xs
+        _ -> header
+
+parseJsonTag :: String -> Parser (String, Char)
+parseJsonTag tag = optional (parseSeparators) *>
+    optional (parseChar '{') *> optional (parseSeparators) *> 
+    parseAnd (parseAfter tag) (parseChar ':')
+    <* optional (parseSeparators)
+
+parseJsonDateTag :: Header -> Parser Header 
+parseJsonDateTag header = parseJsonTag "\"date\""
+    >>= \(x, _) -> optional (parseSeparators) *> parseJsonEntry
+    >>= \(y) -> return header { date = Just (txt y) }
+
+parseJsonAuthorTag :: Header -> Parser Header 
+parseJsonAuthorTag header = parseJsonTag "\"author\""
+    >>= \(x, _) -> optional (parseSeparators) *> parseJsonEntry
+    >>= \(y) -> return header { author = Just (txt y) }
+
+parseJsonTitleTag :: Header -> Parser Header 
+parseJsonTitleTag header = parseJsonTag "\"title\""
+    >>= \(x, _) -> optional (parseSeparators) *> parseJsonEntry
+    >>= \(y) -> return header { title = txt y }
