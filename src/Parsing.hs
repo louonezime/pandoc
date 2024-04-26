d91c51b659937461811b32aa9150af9647ad605a
Lou Onezime <lou.onezime@epitech.eu>
2024-04-26 10:52:56 +0200

    fix(xml): rebase errors and merge conflicts solved

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -127,22 +127,6 @@ parseNonStr str = Parser $ \s ->
         (x : xs) | x `notElem` str -> Right (x, xs)
         _ -> Left (str ++ ": found")
 
-parseCharInStr :: Char -> Parser Char
-parseCharInStr c = Parser $ \str ->
-    case str of
-        (x : xs) -> if c == x
-                    then Right (x, xs)
-                    else runParser (parseCharInStr c) xs
-        _ -> Left (c : ": not found in string")
-
--- parseCharInStr :: Char -> Parser Char
--- parseCharInStr c = Parser $ \str ->
---     case str of
---         (x : xs) -> if c == x
---                     then Right (x, xs)
---                     else runParser (parseCharInStr c) xs
---         _ -> Left (c : ": not found in string")
-
 parseSeparators :: Parser String
 parseSeparators = parseSome (parseAnyChar " \t\n")
 
