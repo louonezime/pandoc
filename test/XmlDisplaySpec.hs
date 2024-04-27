72a48e575d664fc8f66a7c7a6658279ae184965e
MartinFillon <martin.fillon@hotmail.com>
2024-04-27 15:33:54 +0200

    test: test now rebuilds

--- test/XmlDisplaySpec.hs
+++ test/XmlDisplaySpec.hs
@@ -2,8 +2,8 @@ module XmlDisplaySpec (spec) where
 
 import Test.Hspec
 
-import Document (Document (..), Entry (..), Header (..))
 import Display.Xml
+import Document (Document (..), Entry (..), Header (..))
 
 spec :: Spec
 spec = do
@@ -18,27 +18,28 @@ spec = do
             renderHeader (Header "Je connais cette theorie" Nothing Nothing)
                 `shouldBe` "<header title=\"Je connais cette theorie\"></header>"
         it "bold text" $ do
-            renderBold (Bold (Text "Hello")) `shouldBe` "<bold>Hello</bold>"
+            renderBold 0 (Bold (Text "Hello")) `shouldBe` "<bold>Hello</bold>"
         it "italic text" $ do
-            renderItalic (Italic (Text "Hello")) `shouldBe` "<italic>Hello</italic>"
+            renderItalic 0 (Italic (Text "Hello")) `shouldBe` "<italic>Hello</italic>"
         it "code text" $ do
-            renderCode (Code (Text "Hello")) `shouldBe` "<code>Hello</code>"
+            renderCode 0 (Code (Text "Hello")) `shouldBe` "<code>Hello</code>"
         it "paragraph" $ do
-            renderParagraph (Paragraph [Text "Hi", Code (Text "Hello")])
+            renderParagraph 0 (Paragraph [Text "Hi", Code (Text "Hello")])
                 `shouldBe` "<paragraph>Hi<code>Hello</code></paragraph>"
         it "Image" $ do
-            renderImage (Image "www.perdu.com" (Text "No Image"))
+            renderImage 0 (Image "www.perdu.com" (Text "No Image"))
                 `shouldBe` "<image url=\"www.perdu.com\">No Image</image>"
         it "link" $ do
-            renderLink (Link "www.perdu.com" (Text "No Image"))
+            renderLink 0 (Link "www.perdu.com" (Text "No Image"))
                 `shouldBe` "<link url=\"www.perdu.com\">No Image</link>"
         it "List" $ do
-            renderList (List [Paragraph [(Text "www.perdu.com")], Paragraph [(Text "No Image")]])
+            renderList 0 (List [Paragraph [(Text "www.perdu.com")], Paragraph [(Text "No Image")]])
                 `shouldBe` "<list><paragraph>www.perdu.com</paragraph><paragraph>No Image</paragraph></list>"
         it "CodeBlock" $ do
-            renderCodeBlock (CodeBlock [Paragraph [(Text "www.perdu.com")], Paragraph [(Text "No Image")]])
+            renderCodeBlock 0 (CodeBlock [Paragraph [(Text "www.perdu.com")], Paragraph [(Text "No Image")]])
                 `shouldBe` "<codeblock><paragraph>www.perdu.com</paragraph><paragraph>No Image</paragraph></codeblock>"
         it "Section" $ do
             renderSection
+                0
                 (Section "Title" [Paragraph [(Text "www.perdu.com")], Paragraph [(Text "No Image")]])
                 `shouldBe` "<section title=\"Title\"><paragraph>www.perdu.com</paragraph><paragraph>No Image</paragraph></section>"
